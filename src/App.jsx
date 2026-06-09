a2a18f3842832463dac5409065c7edeaf3e9941d
Julisa Rojas <julisa.roci@gmail.com>
2026-06-09 11:14:07 -0500

    feat: add navbar, header, footer and layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import HomePage from "./pages/Home";
 import { CourseProvider } from "./context/CourseContext";
+import Layout from "./components/Layout"
+
 function App() {
   return (
     <CourseProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route pat h="/courses" element={<div>cursos page</div>} />
+          <Route path="/courses" element={<div>cursos page</div>} />
           <Route path="/courses/:id" element={<div>detalle de curso page</div>} />
-          <Route pat h="/enrollments" element={<div>inscripciones page</div>} />
+          <Route path="/enrollments" element={<div>inscripciones page</div>} />
         </Routes>
       </BrowserRouter>
     </CourseProvider>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import Navbar from "./navbar";
+
+export default function Footer() {
+    return (
+        <footer>
+            <h1>Pie de página</h1>
+        </footer>
+    );
+}
